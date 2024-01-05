51bc50c09af7a92b9b32f2c78a5b139cc0109540
Mark John Apostol <m.apostol@centrala.kaczmarski.pl>
2024-01-05 13:57:15 +0800

    simplify content

--- techexamapi/fetch.php
+++ techexamapi/fetch.php
@@ -1,16 +1,15 @@
 <?php
 header("Content-Type:application/json");
 include('db.php');
-$result = mysql_query("SELECT Id, Name FROM tblnames order by id desc",$con);
+$result = mysql_query("SELECT Id, Name FROM tblnames order by id desc limit 1",$con);
 if($result)
 {	
-	$data = array();
-	while ($row = mysql_fetch_array($result))
-	{
-		$name = $row['Name'];
-		$id = $row['Id'];
-		$data[] = response($id, $name); 
-	}
+	$row = mysql_fetch_assoc($result);
+	
+	$name = $row['Name'];
+	$id = $row['Id'];
+	$data = response($id, $name); 
+	
 	echo json_encode($data);
 	mysql_close($con);
 }
